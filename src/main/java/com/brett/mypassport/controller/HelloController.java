99a52225ddbd32c818e749d147ed71cf093a8869
Brett <12744007+brettoak@users.noreply.github.com>
2026-01-30 12:58:22 +1100

    Update HelloController.java

--- src/main/java/com/brett/mypassport/controller/HelloController.java
+++ src/main/java/com/brett/mypassport/controller/HelloController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
+    @Operation(summary = "Get hello message", description = "Returns a JSON object containing a greeting message and status.")
     @GetMapping("/hello")
     public String hello() {
         return """
