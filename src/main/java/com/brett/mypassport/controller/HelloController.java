9e6b8e5c5f8237d630163f33d480517d1f3c9c42
Brett <12744007+brettoak@users.noreply.github.com>
2026-01-30 12:55:35 +1100

    Update HelloController.java

--- src/main/java/com/brett/mypassport/controller/HelloController.java
+++ src/main/java/com/brett/mypassport/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.brett.mypassport.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello, MyPassport!";
+        return """
+                {
+                  "message": "Hello, World!",
+                  "status": "success"
+                }
+                """;
     }
 }
