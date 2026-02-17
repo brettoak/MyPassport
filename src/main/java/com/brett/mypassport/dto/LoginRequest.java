c44da42a158331bc436f128dd49eb5ec38f01d94
Brett <12744007+brettoak@users.noreply.github.com>
2026-02-17 17:06:44 +1100

    Update LoginRequest.java

--- src/main/java/com/brett/mypassport/dto/LoginRequest.java
+++ src/main/java/com/brett/mypassport/dto/LoginRequest.java
@@ -1,7 +1,12 @@
 package com.brett.mypassport.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class LoginRequest {
+    @Schema(description = "User email address", example = "user@example.com")
     private String email;
+    
+    @Schema(description = "User password", example = "123")
     private String password;
 
     // Getters and Setters
